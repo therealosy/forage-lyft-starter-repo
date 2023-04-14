e00f6bd7b29ed121b1e927b2468992be2cfb968c
Osy <40663679+therealosy@users.noreply.github.com>
2023-04-15 00:04:44 +0100

    created abstract class definitions for car

---
+++ car/car.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self):
+        return self.engine.needs_service() or self.battery.needs_service()
